38ac36b4a9fdd2533c2dfafd2602038d077e2c41
Saaiil Basnet <basnetssahil@gmail.com>
2025-05-05 09:44:18 +0545

    connection string secured

--- Backend/database/connection.js
+++ Backend/database/connection.js
@@ -7,7 +7,7 @@ const {Sequelize, DataTypes} = require('sequelize') //destructed elements
 
 const sequelize = new Sequelize("postgresql://postgres.nxpfjywtyjhyvyrcbmzu:[Password_Here]@aws-0-ap-south-1.pooler.supabase.com:6543/postgres") // class instantion and object creation
 
-sequelize.authenticate()
+sequelize.authenticate() //authentication to connect
 .then(()=>{
     console.log("Authenticated and Connected successfully!");
     
